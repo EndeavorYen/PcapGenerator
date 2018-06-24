9bd1be3df7858679ec216ce1c77890a33f1ff152
EndeavorYen <endeavorisforever@gmail.com>
2018-06-24 23:59:43 +0800

    Fixed Pcap Format (UDP --> ICMP)

--- PktConfGenerator.sh
+++ PktConfGenerator.sh
@@ -1,10 +1,13 @@
 #!/bin/bash
 
+# pkt_buf="00 00 00 00 00 00 00 00 00 00 00 00 \
+#          08 00 45 00 00 1c 00 01 00 00 40 11 \
+#          b6 25 02 01 01 02 c0 a8 01 00 13 88 13 88"
 pkt_buf="00 00 00 00 00 00 00 00 00 00 00 00 \
-         08 00 45 00 00 1c 00 01 00 00 40 11 \
-         b6 25 02 01 01 02 c0 a8 01 00 13 88 13 88"
-
-udp_len_checksum=" 00 88 14 23"
+         08 00 45 00 00 2a e0 5e 40 00 40 01 \
+         8e c0 64 56 01 6c 64 56 01 6a 08 00 \
+         b6 08 7a 8b 00 01 00 00 00 00 72 97 \
+         06 00 00 00 00 00 10 11"
 
 base_size=42
 
@@ -28,11 +31,19 @@ output2="2-"$pkt_size"B-"$n_flows"flows.cfg"
 rm $output1
 rm $output2
 
-# TO DO : Fixed UDP Payload Error
-pkt_buf=$pkt_buf$udp_len_checksum
-echo $base_size
-echo  $pkt_size
-for i in `seq $base_size $(( pkt_size - 4 - 1))`; do
+ip_len=$(( pkt_size - 4 - 10 - 4 )) # e.g. 64Byte - 4Byte - 10Byte
+ip_len=($(printf "%04x" $ip_len))
+ip_len=$(echo ${ip_len:0:2} ${ip_len:2:4})
+pkt_buf="00 00 00 00 00 00 00 00 00 00 00 00 \
+         08 00 45 00 $ip_len e0 5e 40 00 40 01 \
+         8e c0 64 56 01 6c 64 56 01 6a 08 00 \
+         b6 08 7a 8b 00 01 00 00 00 00 72 97 \
+         06 00 00 00 00 00 10 11"
+# Instead of ip_len
+# pkt_buf=$(echo ${pkt_buf:0:15})" "$ip_len" "$(echo ${pkt_buf:19:55})
+
+# echo $pkt_buf
+for i in `seq $base_size $(( pkt_size - 10 - 4 - 1 - 4 ))`; do
 	pkt_buf=$pkt_buf" 00"
 done
 
